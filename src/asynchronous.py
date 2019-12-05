c7800d2721e9c7b4baec01ffe1594488071adcd3
Hulian Kling <julian.kling.work@gmail.com>
2019-12-05 21:28:05 +0100

    Changes to make_video and make_images

--- src/asynchronous.py
+++ src/asynchronous.py
@@ -717,11 +717,10 @@ class Worker:
         self.pipe.send("{} going IDLE".format(self.name))
 
     def make_video(self, path, n_episodes, training=False):
-        """Generates a video, to be stored under path, consisting of n_episodes
-        """
+        """Generates image collage, to be stored under path, consisting of n_episodes"""
         fetches = self.greedy_actions_indices if not training else self.sampled_actions_indices
         rectangles = [(160 - 16 * r, 120 - 16 * r, 160 + 16 * r, 120 + 16 * r) for r in self.ratios]
-        print("{} will store the video under {}".format(self.name, path))
+        print("{} will store the images under {}".format(self.name, path))
         with get_writer(path, fps=25, format="mp4") as writer:
             for episode_number in range(n_episodes):
                 print("{} episode {}/{}".format(self.name, episode_number + 1, n_episodes))
@@ -732,7 +731,7 @@ class Worker:
                     vergence_error = self.environment.robot.get_vergence_error(object_distance)
                     frame = make_frame(left_image, right_image, object_distance, vergence_error, episode_number + 1, n_episodes, rectangles)
                     writer.append_data(frame)
-                    if iteration == 0:
+                    if iteration == 0 or iteration == len(self.episode_length)-1:
                         for i in range(24):
                             writer.append_data(frame)
                     feed_dict = {self.left_cam: [left_image], self.right_cam: [right_image]}
@@ -743,23 +742,23 @@ class Worker:
         self.pipe.send("{} going IDLE".format(self.name))
 
     def make_images(self, path, n_episodes, training=False):
-        """Generates a video, to be stored under path, consisting of n_episodes
-        """
+        """Generates a video, to be stored under path, consisting of n_episodes"""
         fetches = self.greedy_actions_indices if not training else self.sampled_actions_indices
         rectangles = [(160 - 16 * r, 120 - 16 * r, 160 + 16 * r, 120 + 16 * r) for r in self.ratios]
         print("{} will store the video under {}".format(self.name, path))
         for episode_number in range(n_episodes):
-            print("[]: {} episode {}/{}".format("make_images", self.name, episode_number + 1, n_episodes))
+            print("{}: {} episode {}/{}".format("make_images", self.name, episode_number + 1, n_episodes))
             self.environment.episode_reset()
             for iteration in range(self.episode_length):
                 left_image, right_image = self.environment.robot.get_vision()
-                object_distance = self.environment.screen.distance
-                vergence_error = self.environment.robot.get_vergence_error(object_distance)
-                frame = make_frame(left_image, right_image, object_distance, vergence_error, episode_number + 1,
-                                   n_episodes, rectangles)
-                image = Image.fromarray(frame)
-                filename = path + "name.jpg"
-                image.save(filename)
+                if iteration == 0 or iteration == (len(self.episode_length)-1):
+                    object_distance = self.environment.screen.distance
+                    vergence_error = self.environment.robot.get_vergence_error(object_distance)
+                    frame = make_frame(left_image, right_image, object_distance, vergence_error, episode_number + 1,
+                                       n_episodes, rectangles)
+                    image = Image.fromarray(frame)
+                    filename = path + "/images/" + "{}.jpg".format(episode_number + iteration)
+                    image.save(filename)
                 feed_dict = {self.left_cam: [left_image], self.right_cam: [right_image]}
                 ret = self.sess.run(fetches, feed_dict)
                 self.environment.robot.set_action(self.actions_indices_to_values(ret))
