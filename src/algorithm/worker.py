e625c04b857aa7bb61ac53afd29457ad0e75b55f
Julian Kling <julian.kling.work@gmail.com>
2020-07-10 13:26:32 +0200

    Why do I stage lines...

--- src/algorithm/worker.py
+++ src/algorithm/worker.py
@@ -253,7 +253,7 @@ class Worker:
         if mt_env:
             self.environment = Environment_MT(headless=task_index != 0 or not worker0_display)
         else:
-        self.environment = Environment(headless=task_index != 0 or not worker0_display)
+            self.environment = Environment(headless=task_index != 0 or not worker0_display)
         print("{} starting V-Rep ... done.".format(self.name))
 
     def add_summary(self, summary, global_step):
